60ca84da12bcf0718f3f61269caa89eda04344de
Pascal Sommer <p@pascalsommer.ch>
2018-02-17 00:32:53 +0100

    formatter

---
+++ influxdbformatter.sh
@@ -0,0 +1,10 @@
+formatter='
+/ p /{print "pos x=" $3 ",y=" $4 ",z=" $5 ",z2=" $6, $1}
+/ a /{print "acc x=" $3 ",y=" $4 ",z=" $5, $1}
+/ g /{print "gyro x=" $3 ",y=" $4 ",z=" $5, $1}
+/ v /{print "vel x=" $3 ",y=" $4 ",z=" $5 ",z2=" $6, $1}
+/ r /{print "rot x=" $3 ",y=" $4 ",z=" $5 ",w=" $6, $1}
+'
+
+awk 'BEGIN{factor=1000000000; t=systime()*factor;}{$1 = t + int($1*factor); print $0}' \
+| awk "$formatter"
