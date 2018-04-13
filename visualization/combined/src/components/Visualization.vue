09b7b0b9529ae4282b09d5248852fbcecb33f7a4
Pascal Sommer <p@pascalsommer.ch>
2018-04-13 11:18:07 +0200

    separate rocketViz from data

--- visualization/combined/src/components/Visualization.vue
+++ visualization/combined/src/components/Visualization.vue
@@ -9,36 +9,60 @@
 import RocketViz from '../visualization.js';
 import * as THREE from 'three';
 import OrbitControls from 'orbit-controls-es6'
-
+import Flightdata from '../flightdata/mestral.js';
 export default {
   name: 'Visualization',
   data () {
     return {
       rocketviz: null,
+      rocketdata: this.formatIFDBData(Flightdata),
     }
   },
-  mounted() {
+  mounted () {
     this.rocketviz = new RocketViz(this.$refs.visualization);
-    this.rocketviz.animate();
 
-    // var el = this.$refs.visualization;
-    // var camera = new THREE.PerspectiveCamera( 70, el.clientWidth / el.clientHeight, 1, 1000 );
-    // camera.position.z = 400;
-    // var scene = new THREE.Scene();
-    // var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-    // var material = new THREE.MeshBasicMaterial( { color: '#cea' } );
-    // var mesh = new THREE.Mesh( geometry, material );
-    // scene.add( mesh );
-    // var renderer = new THREE.WebGLRenderer();
-    // renderer.setPixelRatio( window.devicePixelRatio );
-    // renderer.setSize( el.clientWidth, el.clientHeight );
-    // el.appendChild( renderer.domElement );
+    var i = 0;
+    var scope = this;
+    function render () {
+      requestAnimationFrame( render );
 
-    // mesh.rotation.x += 0.2;
-    // mesh.rotation.y += 0.5;
+      const step = 10;
+      if (i + step > scope.rocketdata.length) {
+        scope.rocketviz.reset();
+        i = 0;
+      }
 
-    // renderer.render( scene, camera );
+      scope.rocketviz.render(scope.rocketdata.slice(i, i + step));
+      i += step;
+    }
 
+    render();
+
+    //this.rocketviz.animate();
+
+  },
+  methods: {
+    formatIFDBData (ifdb_data) {
+      console.log(ifdb_data);
+
+      var out = [];
+
+      ifdb_data.results[0].series[0].values.forEach((arr, i) => {
+        var x, y, z, dump;
+        [dump, x, y, z] = arr;
+
+        out.push({pos: {x: x, y: y, z: z}});
+      });
+
+      ifdb_data.results[0].series[1].values.forEach((arr, i) => {
+        var x, y, z, w, dump;
+        [dump, x, y, z, w] = arr;
+
+        out[i].rot = {x: x, y: y, z: z, w: w};
+      });
+
+      return out;
+    }
   }
 }
 </script>
--- visualization/combined/src/visualization.js
+++ visualization/combined/src/visualization.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import OrbitControls from 'orbit-controls-es6'
-import Flightdata from './flightdata/mestral.js';
 import RocketPartModels from './models/rocket-part-models.js';
 const Stats = require('./stats.js');
 
@@ -20,17 +19,25 @@ function RocketViz(element){
   var stats;
   var firstquat = -1;
 
-  const MAX_POINTS = 10000;
+  const MAX_TRAIL_POINTS = 10000;
+  var current_trail_points = 0;
   const bgcol = 0x151417;
 
   const rocketOffset = new THREE.Vector3(0,0.9,0);
   const cameraOffset = new THREE.Vector3(1,2,4);
 
   // only used for now, will be removed later
-  var i = 0;
-  const rotdata = Flightdata;
+  // var i = 0;
+  // const rotdata = Flightdata;
 
-  // public methods
+  /////////// public methods
+
+  /**
+   *  Clear data for restarting flight
+   */
+  this.reset = () => {
+    current_trail_points = 0;
+  }
 
   /**
    * Render the rocket at its new trajectory point, complete the trajectory
@@ -41,6 +48,7 @@ function RocketViz(element){
                     format: [{pos: {x,y,z}, rot: {x,y,z,w}}]
    */
   this.render = (points) => {
+    //stats.begin();
     if(points.length == 0){
       return;
     }
@@ -65,92 +73,53 @@ function RocketViz(element){
     quat.premultiply(new THREE.Quaternion(-0.7071, 0, 0, 0.7071));
     rocket.quaternion.set(quat.x, quat.y, quat.z, quat.w);
 
+    rocket.position.set(latest.pos.x, latest.pos.y, latest.pos.z);
+    rocket.position.add(rocketOffset);    
 
     // update trail
 
-    //const positions = points.map(p => p.pos);
-
     var positions = trajectory_line.geometry.attributes.position.array;
 
-    points.forEach(p => {
-      // check if positions array is full
-      // yes -> empty array
+    if(current_trail_points + points.length > MAX_TRAIL_POINTS){
+      // move a bit from the end of the line back to start, discard rest.
+      // maybe change this to a smaller fraction if this takes too long.
 
-      // push back like this
-      positions[i * 3] = px;
-      positions[i * 3 + 1] = py;
-      positions[i * 3 + 2] = pz;
-    });
-    trajectory_line.geometry.setDrawRange(0, i+1);
+      var keep_amount = MAX_TRAIL_POINTS / 4;
 
-    // https://stackoverflow.com/a/36498386 this had me very confused for a while...
-    trajectory_line.geometry.computeBoundingSphere();
-    trajectory_line.geometry.attributes.position.needsUpdate = true;
+      // this assumes that MAX_TRAIL_POINTS >> points.length
+      for(var i = 0; i < keep_amount; i++){
+        const old_idx = 3 * (i + current_trail_points - keep_amount);
+        const new_idx = 3 * i;
+        positions[new_idx] = positions[old_idx];
+        positions[new_idx + 1] = positions[old_idx + 1];
+        positions[new_idx + 2] = positions[old_idx + 2];
+      }
 
-  }
-
-  this.animate = () => {
-    requestAnimationFrame( scope.animate );
-    //stats.begin();
-    
-    var rx, ry, rz, rw;
-    var px, py, pz, pz2;
-
-    [rx, ry, rz, rw] = rotdata.results[0].series[1].values[i].slice(1);
-    [px, py, pz, pz2, pz2] = rotdata.results[0].series[0].values[i].slice(1);
-
-    [px, py, pz, pz2] = [px || 0, py || 0, pz || 0, pz2 || 0];
-
-    // everyone who ever decided in favour of a y-up coordinate system on anything should be arrested right now
-    // https://www.gamedev.net/forums/topic/374728-quaternion-rotation-z-up-to-y-up/?do=findComment&comment=3471926
-
-    //document.getElementById('altitude1').innerHTML = pz.toFixed(2) + 'm';
-    //document.getElementById('altitude2').innerHTML = pz2.toFixed(2) + 'm';
-
-    var quat = new THREE.Quaternion(rx, ry, rz, rw);
-    if(firstquat === -1){
-      firstquat = quat.clone();
-      firstquat.inverse();
+      current_trail_points = keep_amount;
     }
 
-    //quat.multiply(new THREE.Quaternion(0, 0, -0.7071, 0.7071));
-    //quat.premultiply(firstquat);
+    points.forEach(p => {
+      positions[current_trail_points * 3] = p.pos.x;
+      positions[current_trail_points * 3 + 1] = p.pos.y;
+      positions[current_trail_points * 3 + 2] = p.pos.z;
 
-    quat.multiply(new THREE.Quaternion(0, 0, -0.7071, 0.7071));
-    quat.premultiply(new THREE.Quaternion(-0.7071, 0, 0, 0.7071));
-
-    rocket.quaternion.set(quat.x, quat.y, quat.z, quat.w);
-
-    // transform z-up to y-up
-    [px, py, pz] = [px, pz, -py];
-
-    var positions = trajectory_line.geometry.attributes.position.array;
-
-    positions[i * 3] = px;
-    positions[i * 3 + 1] = py;
-    positions[i * 3 + 2] = pz;
-    trajectory_line.geometry.setDrawRange(0, i+1);
+      current_trail_points++;
+    });
+    trajectory_line.geometry.setDrawRange(0, current_trail_points);
 
     // https://stackoverflow.com/a/36498386 this had me very confused for a while...
     trajectory_line.geometry.computeBoundingSphere();
     trajectory_line.geometry.attributes.position.needsUpdate = true;
 
-    rocket.position.set(px, py, pz);
-    rocket.position.add(rocketOffset);
-
     controls.update();
-    
-    ++i;
-    i%= rotdata.results[0].series[0].values.length;
-    
     renderer.render( scene, camera );
-
     //stats.end();
   }
-  
+
+  //////////// private methods
+
   var scope = this;
 
-  // private methods
   function init(){
     
     const width = scope.element.clientWidth;
@@ -192,7 +161,7 @@ function RocketViz(element){
     // add trajectory
     var line_material = new THREE.LineBasicMaterial({color: 0xffffff });
     var trajectory_line_geo = new THREE.BufferGeometry();
-    var trajectory_line_positions = new Float32Array( MAX_POINTS * 3 ); // 3 values per vertex
+    var trajectory_line_positions = new Float32Array( MAX_TRAIL_POINTS * 3 ); // 3 values per vertex
     trajectory_line_geo.addAttribute( 'position', new THREE.BufferAttribute( trajectory_line_positions, 3 ) );
     trajectory_line = new THREE.Line(trajectory_line_geo, line_material);
     trajectory_line.position.set(rocketOffset.x, rocketOffset.y, rocketOffset.z);
