7dc1c4b3bbcd1bb8a02d2c72bbe2019f1048b43d
Pascal Sommer <p@pascalsommer.ch>
2019-05-02 23:06:31 +0200

    slow down visualization refresh rate

--- visualization/src/config.js
+++ visualization/src/config.js
@@ -6,6 +6,6 @@ export default {
     fetch_url: fetch_url, // url without trailing slash
 
     data_frame_interval: 50, // ms, see influxdb query on server
-    data_frames_per_view_update: 2,
+    data_frames_per_view_update: 4,
     fetch_ahead_time: 30 * 50, // ms
 };
